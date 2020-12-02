ca40eec5e95749ca860bd499ad09b2896fd64ab2
UGUR GUNDOGDU <ugurgundogdu@me.com>
2020-12-02 19:48:22 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -33,13 +33,3 @@ end
 def move(board, position, player)
   board[position] = "#{player}"
 end
-
-ask for input
-get input
-convert input to index
-if index is valid
-  make the move for index
-  show the board
-else
-  ask for input again until you get a valid input
-end
