f2ec944e79e83689eadce1e58fbad41b07f0c581
UGUR GUNDOGDU <ugurgundogdu@me.com>
2020-12-02 20:50:06 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,6 +6,8 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+display_board(board)
+
 def valid_move?(board, index)
   if index.between?(0,8) && !position_taken?(board,index)
     true
