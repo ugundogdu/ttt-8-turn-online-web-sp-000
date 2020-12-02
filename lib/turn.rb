d23bfa82972af3b8f9b3b0a81269df4185075914
UGUR GUNDOGDU <ugurgundogdu@me.com>
2020-12-02 19:50:11 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -19,15 +19,7 @@ def input_to_index(user_input)
 end
 
 def turn(board)
-  puts 'Please enter 1-9:'
-  user_input = gets.strip
-  index = input_to_index(user_input)
-  if valid_move?(board, index)
-    make_move(board, index, current_player(board))
-    display_board(board)
-  else
-    turn(board)
-  end
+  puts "Please enter 1-9:"
 end
 
 def position_taken?(board, index)
