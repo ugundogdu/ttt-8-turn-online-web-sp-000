4475aeb8332cde6056adc83634ffd391db37f375
UGUR GUNDOGDU <ugurgundogdu@me.com>
2020-12-02 21:20:58 +0000

    Done.

---
+++ lib/turn.rb
@@ -0,0 +1,45 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+
+def valid_move?(board, index)
+  if index.between?(0,8) && !position_taken?(board,index)
+    true
+  else
+    false
+  end
+end
+
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  user_input = gets.strip
+  index = input_to_index(user_input)
+  if !valid_move?(board, index)
+    turn(board)
+  else
+  move(board, index, first_player = "X")
+end
+display_board(board)
+end
+
+
+def position_taken?(board, index)
+  if board[index] == "X" || board[index] == "O"
+    true
+  else
+    false
+  end
+end
+
+def move(board, position, player)
+  board[position] = "#{player}"
+end
