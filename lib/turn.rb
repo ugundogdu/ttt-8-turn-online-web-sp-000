e734131e391171bcf92ab552d270c00ecadd4b3b
UGUR GUNDOGDU <ugurgundogdu@me.com>
2020-12-02 19:20:51 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -21,3 +21,11 @@ end
 def turn(board)
   puts "Please enter 1-9:"
 end
+
+def position_taken?(board, index)
+  if board[index] == "X" || board[index] == "O"
+    true
+  else
+    false
+  end
+end
